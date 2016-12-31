2f788267b94a31a37d7321659886b71ec8c93892
grothedev <grothedev@gmail.com>
2016-12-30 19:46:55 -0600

    added about pdf

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -32,7 +32,7 @@
                     <ul class = "nav navbar-nav navbar-left">
                         <li><a href = "/">Home</a></li>
                         <li><a href = "/make-post">Make a Post</a></li>
-                        <li><a href = "/about">About</a></li>
+                        <li><a href = "{{{ asset('/pdf/about.pdf') }}}">About</a></li>
                         <li><a href = "/feedback">Feedback/Contact</a></li>
                     </ul>
 
